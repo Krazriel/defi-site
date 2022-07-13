68ce3b8c85154ea4ca48975918f420e044507af8
Trung Lam <trunglam107@gmail.com>
2022-07-12 22:32:26 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Featured from "./components/Featured";
+import Signup from "./components/Signup";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Featured />
+      <Signup />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
